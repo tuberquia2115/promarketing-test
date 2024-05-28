e724c48a2e9747ed92b05eb4919ffaafcce79c06
devtubercode <tuberquia2115@gmail.com>
2024-05-27 19:34:59 -0500

    feat: add reference to input

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,24 +1,34 @@
-import { InputHTMLAttributes } from "react";
+"use client";
+
+import React, { InputHTMLAttributes } from "react";
+
 import { Checkbox } from "../checkbox/Checkbox";
 import { Radio } from "../radio/Radio";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input = (props: Props) => {
-  const { type, className, ...restProps } = props;
+const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const { onChange, onBlur, name } = props;
 
-  if (type === "checkbox") {
-    return <Checkbox {...restProps} />;
+  console.log("props", props);
+  if (props.type === "checkbox") {
+    return <Checkbox {...props} />;
   }
 
-  if (type === "radio") {
-    return <Radio {...restProps} />;
+  if (props.type === "radio") {
+    return <Radio {...props} />;
   }
 
   return (
     <input
+      {...props}
+      name={name}
+      ref={ref}
+      onChange={onChange}
+      onBlur={onBlur}
       className={`${props.className} text-[#909090] hide-calendar-icon w-[355px] relative px-4 py-2 border border-secondary1 rounded-xl focus:outline-none focus:ring-2 focus:ring-secondary1 focus:border-transparent`}
-      {...restProps}
     />
   );
-};
+});
+
+export { Input };
