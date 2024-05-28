313c6307727c60468d50dc2ef664cd3dc0c23564
devtubercode <tuberquia2115@gmail.com>
2024-05-27 19:34:28 -0500

    feat: add function to format currency

---
+++ src/utils/currencyFormat.ts
@@ -0,0 +1,9 @@
+export const currencyFormat = (value: number) => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      
+    }).format(value);
+  };
+  
